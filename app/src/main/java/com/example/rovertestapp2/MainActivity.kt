385425a3328a0ed3863c3e63b8d4c2b8607da589
zmanita <zmmanita2000@gmail.com>
2024-02-04 18:21:14 -0600

    All files in kotlin, basic 3button interactive

--- app/src/main/java/com/example/rovertestapp2/MainActivity.kt
+++ app/src/main/java/com/example/rovertestapp2/MainActivity.kt
@@ -2,10 +2,32 @@ package com.example.rovertestapp2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationBarView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var bottomNavigationView: BottomNavigationView
+    var dataFragment = DataFragment()
+    var cameraFragment = CameraFragment()
+    var controlFragment = ControlFragment()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottomNavigationView = findViewById(R.id.bottom_navigation)
+        supportFragmentManager.beginTransaction().replace(R.id.container, dataFragment).commit()
+        bottomNavigationView.setOnItemReselectedListener(NavigationBarView.OnItemReselectedListener { item ->
+            val itemId = item.itemId
+            if (itemId == R.id.data) {
+                supportFragmentManager.beginTransaction().replace(R.id.container, dataFragment)
+                    .commit()
+            } else if (itemId == R.id.camera) {
+                supportFragmentManager.beginTransaction().replace(R.id.container, cameraFragment)
+                    .commit()
+            } else if (itemId == R.id.control) {
+                supportFragmentManager.beginTransaction().replace(R.id.container, controlFragment)
+                    .commit()
+            }
+        })
     }
 }
+
